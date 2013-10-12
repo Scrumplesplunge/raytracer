d0f6cbc70958cf3c811e6dccba742dacb72d2e9b
Joe Fowler <joe.fowler@cs.ox.ac.uk>
2013-10-12 18:34:29 +0100

    Made a (feeble) start on adding a KDTree class, but it's better than nothing..

---
+++ src/KDTree.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Vector.h"
+
+class KDTree : public Shape {
+	private:
+		real split;
+		enum SplitDirection {
+			SPLIT_X,
+			SPLIT_Y
+			SPLIT_Z
+		} splitDirection;
+	public:
+		
+};
+
