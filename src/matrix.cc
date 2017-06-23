653f232f05003c702c3ac53a61f12b80a5f8696e
Joe Fowler <fowlerj@google.com>
2017-06-23 16:36:31 +0100

    Tidied matrix.{h,cc}.

--- src/matrix.cc
+++ src/matrix.cc
@@ -2,50 +2,44 @@
 
 #include <cmath>
 
-Matrix::Matrix() : a{1, 0, 0}, b{0, 1, 0}, c{0, 0, 1}, d{0, 0, 0} {}
-
-Matrix::Matrix(Vector p, Vector q, Vector r, Vector s)
-    : a(p), b(q), c(r), d(s) {}
-
-Matrix Matrix::translate(Vector t) const {
-  return Matrix(a, b, c, d + t);
+Matrix Matrix::translate(Vector offset) const {
+  return {a, b, c, d + offset};
 }
 
-Matrix Matrix::rotateX(real ang) const {
-  real ca = cos(ang), sa = sin(ang);
+Matrix Matrix::rotateX(real radians) const {
+  real ca = cos(radians), sa = sin(radians);
   Vector q = {0, ca, -sa}, r = {0, sa, ca};
-  return Matrix({a.x, dot(q, a), dot(r, a)},
-                {b.x, dot(q, b), dot(r, b)},
-                {c.x, dot(q, c), dot(r, c)},
-                {d.x, dot(q, d), dot(r, d)});
+  return {{a.x, dot(q, a), dot(r, a)},
+          {b.x, dot(q, b), dot(r, b)},
+          {c.x, dot(q, c), dot(r, c)},
+          {d.x, dot(q, d), dot(r, d)}};
 }
 
-Matrix Matrix::rotateY(real ang) const {
-  real ca = cos(ang), sa = sin(ang);
+Matrix Matrix::rotateY(real radians) const {
+  real ca = cos(radians), sa = sin(radians);
   Vector p = {ca, 0, -sa}, r = {sa, 0, ca};
-  return Matrix({dot(p, a), a.y, dot(r, a)},
-                {dot(p, b), b.y, dot(r, b)},
-                {dot(p, c), c.y, dot(r, c)},
-                {dot(p, d), d.y, dot(r, d)});
+  return {{dot(p, a), a.y, dot(r, a)},
+          {dot(p, b), b.y, dot(r, b)},
+          {dot(p, c), c.y, dot(r, c)},
+          {dot(p, d), d.y, dot(r, d)}};
 }
 
-Matrix Matrix::rotateZ(real ang) const {
-  real ca = cos(ang), sa = sin(ang);
+Matrix Matrix::rotateZ(real radians) const {
+  real ca = cos(radians), sa = sin(radians);
   Vector p = {ca, -sa, 0}, q = {sa, ca, 0};
-  return Matrix({dot(p, a), dot(q, a), a.z},
-                {dot(p, b), dot(q, b), b.z},
-                {dot(p, c), dot(q, c), c.z},
-                {dot(p, d), dot(q, d), d.z});
+  return {{dot(p, a), dot(q, a), a.z},
+          {dot(p, b), dot(q, b), b.z},
+          {dot(p, c), dot(q, c), c.z},
+          {dot(p, d), dot(q, d), d.z}};
 }
 
-Vector Matrix::operator()(Vector v) const {
-  return {a.x * v.x + b.x * v.y + c.x * v.z,
-          a.y * v.x + b.y * v.y + c.y * v.z,
-          a.z * v.x + b.z * v.y + c.z * v.z};
+Vector Matrix::operator()(Vector vector) const {
+  return vector.x * a + vector.y * b + vector.z * c;
 }
 
-Vector operator*(const Matrix& m, Vector v) {
-  return {m.a.x * v.x + m.b.x * v.y + m.c.x * v.z + m.d.x,
-          m.a.y * v.x + m.b.y * v.y + m.c.y * v.z + m.d.y,
-          m.a.z * v.x + m.b.z * v.y + m.c.z * v.z + m.d.z};
+Vector operator*(const Matrix& matrix, Vector vector) {
+  return vector.x * matrix.a +
+         vector.y * matrix.b +
+         vector.z * matrix.c +
+         matrix.d;
 }
--- src/matrix.h
+++ src/matrix.h
@@ -3,21 +3,25 @@
 #include "config.h"
 #include "vector.h"
 
-class Matrix {
- public:
-  Vector a, b, c, d;
-
- public:
-  Matrix();
-  Matrix(Vector, Vector, Vector, Vector);
-
-  Matrix translate(Vector) const;
-  Matrix rotateX(real) const;
-  Matrix rotateY(real) const;
-  Matrix rotateZ(real) const;
+struct Matrix {
+  Matrix translate(Vector offset) const;
+  Matrix rotateX(real radians) const;
+  Matrix rotateY(real radians) const;
+  Matrix rotateZ(real radians) const;
 
   // Rotate but do not translate.
-  Vector operator()(Vector) const;
+  Vector operator()(Vector direction) const;
+
+  // These vectors are columns of the matrix. The abstract matrix is of the
+  // form:
+  // | a.x b.x c.x d.x |
+  // | a.y b.y c.y d.y |
+  // | a.z b.z c.z d.z |
+  // |  0   0   0   1  |
+  Vector a = {1, 0, 0},
+         b = {0, 1, 0},
+         c = {0, 0, 1},
+         d = {0, 0, 0};
 };
 
-Vector operator*(const Matrix&, Vector);
+Vector operator*(const Matrix& matrix, Vector position);
